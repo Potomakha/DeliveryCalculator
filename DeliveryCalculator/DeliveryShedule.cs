d514cebf5d2bfc276950a4b958f5566ef829b65a
Oleh Potomakha <potomakha@gmail.com>
2022-01-08 13:01:00 +0200

    DeliveryTimeCalculation done

---
+++ DeliveryCalculator/DeliveryShedule.cs
@@ -0,0 +1,9 @@
+﻿namespace DeliveryCalculator
+{
+    public class DeliveryShedule
+    {
+        public int StartHour => 08;
+        public int EndHour => 17;
+        public int[] Weekend => new int[] { 0, 6 };
+    }
+}
--- DeliveryCalculator/Program.cs
+++ DeliveryCalculator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace DeliveryCalculator
 {
@@ -6,7 +7,42 @@ namespace DeliveryCalculator
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Order date");
+            var inputString = Console.ReadLine();
+            var orderDate = DateTime.Parse(inputString);
+            Console.WriteLine("Delivery date");
+            Console.WriteLine(DeliveryTimeCalculation(orderDate).ToString("dd.MM.yyy HH:mm"));
+            Console.ReadKey();
+        }
+
+        public static DateTime DeliveryTimeCalculation(DateTime orderTime)
+        {
+            var shedule = new DeliveryShedule();
+            var deliveryTime = orderTime;
+
+            if (shedule.Weekend.Contains((int)deliveryTime.DayOfWeek))
+            {
+                while (shedule.Weekend.Contains((int)deliveryTime.DayOfWeek))
+                {
+                    deliveryTime = deliveryTime.AddDays(1);
+                }
+
+                deliveryTime = new DateTime(deliveryTime.Year, deliveryTime.Month, deliveryTime.Day, shedule.StartHour, 00, 00);
+            }
+
+            if (deliveryTime.Hour < shedule.StartHour)
+            {
+                deliveryTime = new DateTime(deliveryTime.Year, deliveryTime.Month, deliveryTime.Day, shedule.StartHour, 00, 00);
+            }
+
+            if (deliveryTime.Hour > shedule.EndHour)
+            {
+                deliveryTime = deliveryTime.AddDays(1);
+                deliveryTime = new DateTime(deliveryTime.Year, deliveryTime.Month, deliveryTime.Day, shedule.StartHour, 00, 00);
+            }
+
+            deliveryTime = deliveryTime.AddHours(48);
+            return deliveryTime;
         }
     }
 }
